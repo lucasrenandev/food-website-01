aab4b61e5de6514fc47aa1961a3d47677ef1d218
lucasrenandns <lucasrenandns96@gmail.com>
2023-04-04 22:43:57 -0300

    Updated website

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const menuIcon = document.getElementById("menu-icon")
+const navBar = document.querySelector(".navbar")
+
+menuIcon.addEventListener("click", () => {
+    navBar.classList.toggle("active")
+    menuIcon.classList.toggle("bx-x")
+})
+
+window.addEventListener("scroll", () => {
+    navBar.classList.remove("active")
+    menuIcon.classList.remove("bx-x")
+})
